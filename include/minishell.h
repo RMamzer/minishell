c2eefa6191a2d1b2f6b75c44d38ee1f18c38afd1
Maxim Kleverov <mklevero@student.hive.fi>
2025-08-06 18:30:58 +0300

    PARSING: started lexer

--- include/minishell.h
+++ include/minishell.h
@@ -3,10 +3,43 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rmamzer <rmamzer@student.hive.fi>          +#+  +:+       +#+        */
+/*   By: mklevero <mklevero@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/06 14:59:35 by rmamzer           #+#    #+#             */
-/*   Updated: 2025/08/06 14:59:36 by rmamzer          ###   ########.fr       */
+/*   Updated: 2025/08/06 18:28:51 by mklevero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include "libft.h"
+# include <stdbool.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+# define SUCCESS 0
+# define FAILURE 1
+
+# define TO_TRIM " \t\n\v\f\r"
+
+// core
+typedef struct s_shell
+{
+	int	exit_code;
+
+}		t_shell;
+
+// token type
+typedef enum e_token_type
+{
+	PIPE,
+	IN,
+	OUT,
+	WORD,
+	HEREDOC,
+	APPEND
+
+}		t_token_type;
+
+#endif
--- src/main.c
+++ src/main.c
@@ -6,16 +6,45 @@
 /*   By: mklevero <mklevero@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/06 14:54:37 by rmamzer           #+#    #+#             */
-/*   Updated: 2025/08/06 15:13:14 by mklevero         ###   ########.fr       */
+/*   Updated: 2025/08/06 18:28:49 by mklevero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	main(void)
+#include "minishell.h"
+
+int	main(int ac, char **av, char **env)
 {
-	int test;
-	test = 322;
+	char	*input;
+	t_shell	*data;
 
-	int solo;
+	input = NULL;
+	data = init_data();
+	process_input(input, data);
+}
+t_shell	*init_data(void)
+{
+	t_shell	*data;
 
-	solo = 228;
+	data = malloc(sizeof(t_shell));
+	if (data == NULL)
+		return (NULL);    // for now null
+	data->exit_code = -1; // for now
+}
+bool	process_input(char *input, t_shell *data)
+{
+	char	*trimmed_input;
+
+	trimmed_input = ft_strtrim(input, TO_TRIM);
+	if (trimmed_input == NULL)
+		return (NULL); // for now
+	free(input);
+	lexer(trimmed_input, data);
+}
+
+void	lexer(char *trimmed_input, t_shell *data)
+{
+	// loop through the input
+	// find smth
+	// extract
+	// create node, add type, add to list
 }
