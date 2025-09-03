704d323004ee119e947bdcafc619ea6aa06a39d4
Maxim Kleverov <maximkleverov@icloud.com>
2025-09-03 12:18:40 +0300

    MAX: header file checked

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: mklevero <mklevero@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/06 14:59:35 by rmamzer           #+#    #+#             */
-/*   Updated: 2025/09/02 15:55:30 by mklevero         ###   ########.fr       */
+/*   Updated: 2025/09/03 12:07:16 by mklevero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -119,9 +119,9 @@ bool				check_syntax(t_shell *shell);
 
 // expansion
 void				expander(t_shell *shell);
-char				*expand_content(char *content, t_shell *shell);
-char				*process_content(char *content, size_t *i, t_shell *shell);
-char				*handle_dollar(char *content, size_t *i, t_shell *shell);
+char	*expand_content(char *content, t_shell *shell, t_token *token);
+char	*process_content(char *content, size_t *i, t_shell *shell, t_token *token);
+char	*handle_dollar(char *content, size_t *i, t_shell *shell, t_token *token);
 char				*handle_characters(char *content, size_t *i, bool in_dq);
 char				*expand_env_var(char *content, size_t *i, t_env *env);
 char				*get_env_value(char *name, t_env *env, bool alloc);
@@ -129,8 +129,7 @@ char				*strjoin_free(char *new_content, char *temp);
 int					ft_strcmp(const char *s1, const char *s2);
 
 char				*handle_single_quote(char *content, size_t *i);
-char				*handle_double_quote(char *content, size_t *i,
-						t_shell *shell);
+char	*handle_double_quote(char *content, size_t *i, t_shell *shell, t_token *token);
 
 // helper functions
 bool				is_delimiter(int i);
