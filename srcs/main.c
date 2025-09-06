5fbc722c71690f10eec2e8bf84a83e64804898ab
Maxim Kleverov <maximkleverov@icloud.com>
2025-09-06 13:49:18 +0300

    MAX: new test ast added

--- srcs/main.c
+++ srcs/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rmamzer <rmamzer@student.hive.fi>          +#+  +:+       +#+        */
+/*   By: mklevero <mklevero@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/06 14:54:37 by rmamzer           #+#    #+#             */
-/*   Updated: 2025/09/04 18:04:02 by rmamzer          ###   ########.fr       */
+/*   Updated: 2025/09/06 13:37:15 by mklevero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,14 +23,14 @@
 // TODO: ERROR MANAGEMENT
 
 // test function, remove later
-// void	test_tokens(t_token *list)
-// {
-// 	while (list)
-// 	{
-// 		printf("[TYPE: %d] \"%s\"\n", list->type, list->content);
-// 		list = list->next;
-// 	}
-// }
+void	test_tokens(t_token *list)
+{
+	while (list)
+	{
+		printf("[TYPE: %d] \"%s\"\n", list->type, list->content);
+		list = list->next;
+	}
+}
 
 // void	test_env(t_env *envlist)
 // {
@@ -42,42 +42,60 @@
 // 	}
 // }
 // remove
-const char	*type_to_str(t_token_type t)
+const char *type_to_str(t_token_type t)
 {
-	switch (t)
-	{
-	case WORD:
-		return ("WORD");
-	case IN:
-		return ("IN");
-	case OUT:
-		return ("OUT");
-	case APPEND:
-		return ("APPEND");
-	case HEREDOC:
-		return ("HEREDOC");
-	case PIPE:
-		return ("PIPE");
-	default:
-		return ("UNKNOWN");
-	}
+ switch (t)
+ {
+     case WORD:    return "WORD";
+     case IN:      return "IN";
+     case OUT:     return "OUT";
+     case APPEND:  return "APPEND";
+     case HEREDOC: return "HEREDOC";
+     case PIPE:    return "PIPE";
+     default:      return "UNKNOWN";
+ }
 }
 
-void	print_ast(t_ast *node, int depth)
+static void print_value(char **value)
 {
-	if (!node)
-		return ;
-	// indentation for readability
-	for (int i = 0; i < depth; i++)
-		printf("  ");
-	printf("%s", type_to_str(node->type));
-	if (node->value && node->value[0])
-		printf(" (%s)", node->value[0]); // show filename or command name
-	printf("\n");
-	if (node->left)
-		print_ast(node->left, depth + 1);
-	if (node->right)
-		print_ast(node->right, depth + 1);
+ int i = 0;
+
+ if (!value)
+  return ;
+ printf(" (");
+ while (value[i])
+ {
+  if (i > 0)
+   printf(" ");
+  printf("%s", value[i]);
+  i++;
+ }
+ printf(")");
+}
+
+void print_ast(t_ast *node, int depth)
+{
+ int i;
+
+ if (!node)
+ {
+  printf("ast is clean\n");
+  return ;
+ }
+ /* indentation */
+ i = 0;
+ while (i++ < depth)
+  printf("  ");
+
+ printf("%s", type_to_str(node->type));
+ if (node->value && node->value[0])
+  print_value(node->value);
+ printf("\n");
+
+ if (node->left)
+  print_ast(node->left, depth + 1);
+ if (node->right)
+  print_ast(node->right, depth + 1);
 }
 
 int	main(int ac, char **av, char **env)
@@ -100,14 +118,14 @@ int	main(int ac, char **av, char **env)
 		shell->input_line = input;
 		if (process_input(shell->input_line, shell) == SUCCESS)
 		{
-			// test_tokens(shell->token_list);
+			test_tokens(shell->token_list);
 		}
 		if (parse_tokens(shell) == SUCCESS)
 		{
 			print_ast(shell->ast, 0);
 			execute_ast(shell->ast, shell);
-
 			free_ast(&shell->ast);
+			print_ast(shell->ast, 0);
 			shell->ast = NULL;
 		}
 	}
