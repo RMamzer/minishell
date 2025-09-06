73fd913add153f263913113400ab35a9e02e9540
Maxim Kleverov <maximkleverov@icloud.com>
2025-09-06 12:17:01 +0300

    MAX: AST for test

--- srcs/parser.c
+++ srcs/parser.c
@@ -6,7 +6,7 @@
 /*   By: mklevero <mklevero@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/27 18:05:25 by mklevero          #+#    #+#             */
-/*   Updated: 2025/09/05 11:54:45 by mklevero         ###   ########.fr       */
+/*   Updated: 2025/09/06 12:15:16 by mklevero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,21 +26,264 @@ t_ast	*parse_pipe(t_token **token_list, t_shell *shell)
 	t_ast	*pipe_node;
 	t_token	*pipe_token;
 
-	node = parse_redirection(token_list, shell);
+	node = parse_command_and_redirection(token_list, shell);
 	if (*token_list && (*token_list)->type == PIPE)
 	{
 		pipe_node = add_ast_node(PIPE, shell);
-		pipe_node->left = node;
 		pipe_token = *token_list;
 		*token_list = (*token_list)->next;
 		free(pipe_token->content);
 		free(pipe_token);
+        pipe_node->left = node;
 		pipe_node->right = parse_pipe(token_list, shell);
 		return (pipe_node);
 	}
 	return (node);
 }
+bool is_redir(t_token_type type)
+{
+    return(type == IN || type == OUT || type == APPEND || type == HEREDOC);
+}
+t_ast *parse_command_and_redirection(t_token **token_list, t_shell *shell)
+{
+    t_ast *root;
+    t_ast *tail;
+    t_ast *cmd_node;
+    
+    root = NULL;
+    tail = NULL;
+    cmd_node = NULL;
+    while (*token_list && (is_redir((*token_list)->type) || (*token_list)->type == WORD))
+    {
+        if((*token_list)->type == WORD)
+            cmd_node = handle_word_ast(token_list, cmd_node, shell);
+        else
+            root = handle_redir_ast(token_list, &tail, shell);
+    }
+    if(tail)
+    {
+        tail->left = cmd_node;
+        return (root);
+    }
+    return (cmd_node);
+}
 
+t_ast *handle_word_ast(t_token **token_list, t_ast *cmd_node, t_shell *shell)
+{
+    if(!cmd_node)
+        cmd_node = add_ast_node((*token_list)->type, shell);
+    append_arg(cmd_node, (*token_list)->content, shell);
+    move_and_free(token_list);
+    return (cmd_node);
+}
+
+t_ast *handle_redir_ast(t_token **token_list, t_ast **tail, t_shell *shell)
+{
+    t_token *redir_token;
+    t_token *file_token;
+    t_ast *node;
+    
+    redir_token = *token_list;
+    file_token = redir_token->next;
+    if(!file_token || file_token->type != WORD)
+        fatality("syntax error", shell, 2);
+    node = add_ast_node(redir_token->type, shell);
+    node->right = add_file_node(file_token, shell);
+    *token_list = file_token->next;
+    free(redir_token->content);
+    free(redir_token);
+    
+    if(!*tail)
+        *tail = node;
+    else
+    {
+        (*tail)->left = node;
+        *tail = node;
+    }
+    return (*tail); // mb *node
+}
+
+void append_arg(t_ast *cmd_node, const char *str, t_shell *shell)
+{
+    char **new;
+    size_t i;
+    
+    i = 0;
+    if(!cmd_node->value)
+        return (init_arg(cmd_node, str, shell));
+    while(cmd_node->value[i])
+        i++;
+    new = load_arg(cmd_node->value, i, shell);
+    new[i] = ft_strdup(str);
+    if(!new[i])
+        fatality("malloc failed", shell, 1);
+    new[i + 1] = NULL;
+    cmd_node->value = new;
+}
+void init_arg(t_ast *cmd_node, const char *str, t_shell *shell)
+{
+    cmd_node->value = ft_calloc(2, sizeof(char *));
+    if(!cmd_node->value)
+        fatality("malloc failed", shell, 1);
+    cmd_node->value[0] = ft_strdup(str);
+    if(!cmd_node->value[0])
+        fatality("malloc failed", shell, 1);
+    cmd_node->value[1] = NULL;
+}
+char **load_arg(char **old, size_t count, t_shell *shell)
+{
+    char **new;
+    size_t i;
+    
+    i = 0;
+    new = ft_calloc(count + 2, sizeof(char *));
+    if(!new)
+        fatality("malloc failed", shell, 1);
+    while(i < count)
+    {
+        new[i] = old[i];
+        i++;
+    }
+    free(old);
+    return (new);
+}
+
+
+t_ast	*add_file_node(t_token *token, t_shell *shell)
+{
+	t_ast	*file_node;
+
+	file_node = add_ast_node(token->type, shell);
+	file_node->value = ft_calloc(2, sizeof(char *));
+	if (file_node == NULL)
+		fatality("malloc: Memory allocation failed", shell, 1);
+	file_node->value[0] = ft_strdup(token->content);
+	if (file_node->value[0] == NULL)
+		fatality("malloc: Memory allocation failed", shell, 1);
+	free(token->content);
+	free(token);
+	return (file_node);
+}
+
+
+t_ast	*add_ast_node(t_token_type type, t_shell *shell)
+{
+	t_ast	*node;
+
+	node = ft_calloc(1, sizeof(t_ast));
+	if (!node)
+		fatality("malloc: Memory allocation failed", shell, 1);
+	node->type = type;
+	node->fd[0] = -1;
+	node->fd[1] = -1;
+	return (node);
+}
+
+void move_and_free(t_token **token_list)
+{
+    t_token *temp;
+    
+    if(!token_list || !*token_list)
+        return ;
+    temp = *token_list;
+    *token_list = (*token_list)->next;
+    free(temp->content);
+    free(temp);
+}
+
+/*
+t_ast *parse_command_and_redirection(t_token **token_list, t_shell *shell)
+{
+    t_ast *root;
+    t_ast *last_redir;
+    t_ast *cmd_node;
+    t_ast *redirect_node;
+    t_token *redirect_token;
+    t_token *file_token;
+    t_token *next;
+    
+    
+    root = NULL;
+    last_redir = NULL;
+    cmd_node = NULL;
+    while (*token_list && (is_redir((*token_list)->type) || (*token_list)->type == WORD))
+    {
+        if((*token_list)->type == WORD)
+        {
+            if(!cmd_node)
+                cmd_node = add_ast_node(WORD, shell);
+            append_arg(cmd_node, (*token_list)->content, shell);
+            move_and_free(token_list);
+        }
+        else if(is_redir((*token_list)->type))
+        {
+            redirect_token = *token_list;
+            file_token = redirect_token->next;
+            if(!file_token || file_token->type != WORD)
+                fatality("syntax error", shell, 2);
+            next = file_token->next;
+            redirect_node = add_ast_node(redirect_token->type, shell);
+            redirect_node->right = add_file_node(file_token, shell);
+            
+            free(redirect_token->content);
+            free(redirect_token);
+            *token_list = next;
+            
+            if(!root)
+                root = redirect_node;
+            else
+                last_redir->left= redirect_node;
+            last_redir = redirect_node;
+        }
+        else
+            break;
+    }
+    if(last_redir)
+    {
+        last_redir->left = cmd_node;
+        return (root);
+    }
+    return (cmd_node);
+}
+
+
+
+
+void append_arg(t_ast *cmd_node, const char *str, t_shell *shell)
+{
+    char **new;
+    size_t i;
+    
+    i = 0;
+    if(!cmd_node->value)
+    {
+        cmd_node->value = ft_calloc(2, sizeof(char *));
+        if(!cmd_node->value)
+            fatality("malloc failed", shell, 1);
+        cmd_node->value[0] = ft_strdup(str);
+        if(!cmd_node->value[0])
+            fatality("malloc failed", shell, 1);
+        cmd_node->value[1] = NULL;
+        return ;
+    }
+    while(cmd_node->value[i])
+        i++;
+    new = ft_calloc(i + 2, sizeof(char *));
+    if(!new)
+        fatality("malloc failed", shell, 1);
+    while(i-- > 0)
+        new[i] = cmd_node->value[i];
+    i = 0;
+    while(new[i])
+        i++;
+    new[i] = ft_strdup(str);
+    if(!new[i])
+        fatality("malloc failed", shell, 1);
+    new[i + 1] = NULL;
+    free(cmd_node->value);
+    cmd_node->value = new;
+}
+*/
 
 // FIRST VERSION
 // t_ast	*parse_command_with_redirections(t_token **token_list, t_shell *shell)
@@ -192,61 +435,10 @@ t_ast	*parse_single_redirection(t_token **token_list, t_shell *shell)
 */
 
 
-bool is_redir(t_token_type type)
-{
-    return(type == IN || type == OUT || type == APPEND || type == HEREDOC);
-}
-static t_ast *parse_simple_cmd(t_token **token_list, t_shell *shell)
-{
-    t_ast *root;
-    t_ast *last_redir;
-    t_ast *cmd_node;
-    t_ast *redirect_node;
-    t_token *redirect_token;
-    t_token *file_token;
-    
-    
-    root = NULL;
-    last_redir = NULL;
-    cmd_node = NULL;
-    while (*token_list && (is_redir((*token_list)->type) || (*token_list)->type == WORD))
-    {
-        if((*token_list)->type == WORD)
-        {
-            if(!cmd_node)
-                cmd_node = parse_command(token_list, shell);
-            else
-                break;
-        }
-        else if(is_redir((*token_list)->type))
-        {
-            redirect_token = *token_list;
-            file_token = (*token_list)->next;
-            if(!file_token || file_token->type != WORD)
-                fatality(NULL, shell, 2);
-            redirect_node = add_ast_node(redirect_token->type, shell);
-            redirect_node->right = add_file_node(file_token, shell);
-            *token_list = file_token->next;
-            free(redirect_token->content);
-            free(redirect_token);
-            if(!root)
-                root = redirect_node;
-            else
-                last_redir->left¨= redirect_node;
-            last_redir = redirect_node;
-        }
-        else
-            break;
-    }
-    if(last_redir)
-    {
-        last_redir->left = cmd_node;
-        return (root);
-    }
-    return (cmd_node);
-}
 
 
+
+/*
 int	count_args(t_token *tokens)
 {
 	int	counter;
@@ -293,34 +485,8 @@ t_ast	*parse_command(t_token **token_list, t_shell *shell)
 	return (command_node);
 }
 
-t_ast	*add_file_node(t_token *token, t_shell *shell)
-{
-	t_ast	*file_node;
+*/
 
-	file_node = add_ast_node(token->type, shell);
-	file_node->value = ft_calloc(2, sizeof(char *));
-	if (file_node == NULL)
-		fatality("malloc: Memory allocation failed", shell, 1);
-	file_node->value[0] = ft_strdup(token->content);
-	if (file_node->value[0] == NULL)
-		fatality("malloc: Memory allocation failed", shell, 1);
-	free(token->content);
-	free(token);
-	return (file_node);
-}
-
-t_ast	*add_ast_node(t_token_type type, t_shell *shell)
-{
-	t_ast	*node;
-
-	node = ft_calloc(1, sizeof(t_ast));
-	if (!node)
-		fatality("malloc: Memory allocation failed", shell, 1);
-	node->type = type;
-	node->fd[0] = -1;
-	node->fd[1] = -1;
-	return (node);
-}
 
 // free token list
 void	free_list(t_token **list)
